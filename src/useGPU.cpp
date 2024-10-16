cb2c449288c4cb865843492e2c2e3b68dd32d407
tobiasfremming <tobias.fremming@gmail.com>
2024-10-16 18:42:26 +0200

    feat: play around with cuda

---
+++ src/useGPU.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+using namespace std;
+
+// cuda kernel
+__global__ void add(int* a, int* b, int* c) {
+    int i = threadIdx.x + blockDim.x * blockIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+__managed__ int vector_a[256], vector_b[256], vector_c[256];
+
+int main() {
+    cout << "hello world" << endl;
+    for (int i = 0; i < 256; i++){
+        vector_a[i] = i;
+        vector_b[i] = 256 -9;
+    }
+
+    add<<<1, 256 >>>(vector_a, vector_b, vector_c);
+
+    cudaDeviceSyncronize();
+
+    int result_sum = 0;
+
+    for (int i = 0; i < 256; i++) {
+        result_sum += vector_c[i] 
+    }
+    
+    printf("Result: sum = % d", result_sum);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
